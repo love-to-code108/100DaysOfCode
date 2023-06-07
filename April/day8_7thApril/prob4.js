494af85407f0df6ea72c31125168e7112065d5bd
love-to-code108 <love.to.code108@gmail.com>
2023-06-07 10:37:24 +0530

    adding the file prob4.js

---
+++ April/day8_7thApril/prob4.js
@@ -0,0 +1,18 @@
+//  try to add a new key to the const object in the problem3 were you able to do it.
+
+const a = {
+    92 : 69,
+    "hello": 6969,
+    "world": 68
+};
+
+a[92] = 699;
+a.newKey1 = 1;  // this is called the dot notation 
+a["newKey2"] = 2; // this is called the bracket notation
+console.log(a)
+
+
+//  i am linking the article from which i understood this so feel free to go and do a deep dive  
+
+//  alt click to open this link below
+https://www.freecodecamp.org/newsjavascript-object-keys-tutorial-how-to-use-a-js-key-value-pair/
