ac9e365d09cd677885052d590987b05acf43f066
Biswanath Mukherjee <114847271+love-to-code108@users.noreply.github.com>
2023-06-08 06:50:03 +0530

    Delete April directory

--- April/day5_2ndApril/prob4.py
+++
@@ -1,8 +0,0 @@
-m, n = map(int, input().strip().split())
-design = '.|.'
-
-for i in range(1, m//2 + 1):
-    print((design*(2*i - 1)).center(n, '-'))
-print('WELCOME'.center(n, '-'))
-for i in range(m//2, 0, -1):
-    print((design*(2*i - 1)).center(n, '-'))
--- April/day7_6thApril/input.js
+++
@@ -1,3 +0,0 @@
-const input = require("prompt-sync")();
-a = input("Enter something here to check this thing is working: ");
-console.log("Here is the output: ",a);
