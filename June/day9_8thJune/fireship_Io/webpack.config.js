342f2498b97b79bbea0b772471c25b331bf0b375
love-to-code108 <love.to.code108@gmail.com>
2023-06-08 05:32:04 +0530

    finally was able to change webpack mode to development i was making the mistake in the module.exports i was writing module.export instead of module.exports basically syntax error

--- June/day9_8thJune/fireship_Io/webpack.config.js
+++ June/day9_8thJune/fireship_Io/webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 
-module.export = {
+module.exports = {
+    mode: "development",
     entry: './src/index.js',  // this defines the entry point of your application
     output: {
-        filename: 'awesome.js',
         path: path.resolve(__dirname, 'dist'),
+        filename: 'awesome.js',
     }
 };
