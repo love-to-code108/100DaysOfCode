fc56ee519cc2bb9c23cff89d753835ef92f97856
love-to-code108 <love.to.code108@gmail.com>
2023-06-20 15:40:45 +0530

    modified sixth.js file

--- June/day10_20thJune/sixth.js
+++ June/day10_20thJune/sixth.js
@@ -1,7 +1,7 @@
-const input = require("prompt-sync");
-let age = input("Enter your age: ");
+const input = require("prompt-sync")();
+let age = Number(input("Enter your age: "));
 if(age>10 && age<20){
-    console.log("Your age is between 10 and 20 ",age);
+    console.log("Your age is between 10 and 20",age);
 }
 else{
     console.log("Your age is not between 10 and 20",age);
