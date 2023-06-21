25134744c7df97a9cca3643ed5c03f52a9e86aac
love-to-code108 <love.to.code108@gmail.com>
2023-06-21 17:26:14 +0530

    modified the 2nd.js file

--- June/day11_21stJune/2nd.js
+++ June/day11_21stJune/2nd.js
@@ -1,5 +1,5 @@
 // the for of loop basically iterates through all the characters in a string or all the index of an array
 
 for (let i of "Hello World"){
-    console.log(i)
+    console.log(i);
 }
