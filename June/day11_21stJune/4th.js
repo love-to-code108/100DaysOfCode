299e573f2a90e517570279e1e20371bcb62174ee
love-to-code108 <love.to.code108@gmail.com>
2023-06-21 17:54:27 +0530

    modified 4th.js file

--- June/day11_21stJune/4th.js
+++ June/day11_21stJune/4th.js
@@ -1,1 +1,16 @@
+const obj = {
+    "Ahmed Payne":10,
+    'Markus Yoder':20,
+    'Pablo Raymond':30,
+    'Kayley Buchanan':40,
+    'Kayla Waller':50,
+    'Sullivan Bolton':60,
+    'Alisha Pierce':70,
+    'June Cooper':80,
+    'Salvador Buchanan':90,
+    'Chanel Mcconnell':100,
+    'Zechariah Thornton':87,
+    'Maleah Nash':89
+}
+
 
