07969bff7f456e74a29850104fafbdbf40746621
love-to-code108 <love.to.code108@gmail.com>
2023-06-21 18:34:46 +0530

    modified 6th.js file

--- June/day11_21stJune/6th.js
+++ June/day11_21stJune/6th.js
@@ -5,6 +5,8 @@ let c = Number(input("Enter a number: "));
 let d = Number(input("Enter a number: "));
 let e = Number(input("Enter a number: "));
 
+
+// this is what we like to call an arrow function 
 const mean = (a1,b1,c1,d1,e1)=>{
     return((a1+b1+c1+d1+e1)/5);
 }
