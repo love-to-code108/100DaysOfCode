a39c7f6d398f14a099d764b532e17829d6042aa9
love-to-code108 <love.to.code108@gmail.com>
2023-06-22 10:55:40 +0530

    modified the 1st.js file

--- June/day12_22ndJune/1st.js
+++ June/day12_22ndJune/1st.js
@@ -5,8 +5,21 @@ for(let i of arr){
     console.log(i);
 }
 
+// printing the index numbers of the array:
+for(let i in arr){
+    console.log(i);
+}
+
+
+
 console.log("Printing the array using basic for loop");
 // printing the array using for loop 
 for(let i=0;i<arr.length;i++){
     console.log(arr[i]);
 }
+
+
+
+
+// printing the whole array at once 
+console.log(arr)
