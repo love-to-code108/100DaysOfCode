9fd6a33bef3b1885be07ba84c4c68823b88981be
love-to-code108 <love.to.code108@gmail.com>
2023-06-22 10:53:55 +0530

    modified file 1st.js

--- June/day12_22ndJune/1st.js
+++ June/day12_22ndJune/1st.js
@@ -1,6 +1,12 @@
+// printing an array using for of loop
+
 let arr = [1,2,3,4,5,6];
 for(let i of arr){
     console.log(i);
 }
 
-// printing an array using for of loop
+console.log("Printing the array using basic for loop");
+// printing the array using for loop 
+for(let i=0;i<arr.length;i++){
+    console.log(arr[i]);
+}
