b1d34716cde689710d61f76edfa91e345991cf99
love-to-code108 <love.to.code108@gmail.com>
2023-06-22 15:25:14 +0530

    modified the 4th.js file

--- June/day12_22ndJune/4th.js
+++ June/day12_22ndJune/4th.js
@@ -2,9 +2,11 @@
 const input = require("prompt-sync")();
 
 let arr = [1,3,4];
-n = Number(input("Enter The number of numbers you want to input: "));
+let n = Number(input("Enter The number of numbers you want to input: "));
 for(let i=0;i<n;i++){
-    arr.push(Number(input("Enter here: ")));
+    
+    let num = Number(input("Enter here: "))
+    arr.push(num);
 }
 
 console.log(arr)
