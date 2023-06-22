0da6a87d5a6cf3a8d8f6fabf2ee7657411723076
love-to-code108 <love.to.code108@gmail.com>
2023-06-22 15:30:05 +0530

    modified the 5th.js file

--- June/day12_22ndJune/5th.js
+++ June/day12_22ndJune/5th.js
@@ -1,11 +1,17 @@
 const input = require("prompt-sync")();
 
 let arr = [1,3,4];
-let n = Number(input("Enter The number of numbers you want to input: "));
-for(let i=0;i<n;i++){
+// let n = Number(input("Enter The number of numbers you want to input: "));
+let n = -1;
+for(let i=0;i>n;i++){
     
     let num = Number(input("Enter here: "))
-    arr.push(num);
+    if(num === 0){
+        arr.push(num);
+        break;
+    }else{
+        arr.push(num);
+    }
 }
 
-console.log(arr)
+console.log(arr);
